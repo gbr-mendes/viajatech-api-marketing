50625b67dee617cfc8b4dd7f0f5d2a4e830a6d3c
Gabriel <gabrielx165@gmail.com>
2022-06-29 12:21:29 -0300

    setup dotenv

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,11 @@
+import os
 from app.main import app
+from dotenv import load_dotenv
+
+load_dotenv()
 
 if __name__ == '__main__':
-    app.run()
+    PORT = os.getenv('PORT')
+    DEBUG = os.getenv('DEBUG')
+    print(f"App listening at port {PORT}...")
+    app.run(port=PORT, debug=DEBUG)
