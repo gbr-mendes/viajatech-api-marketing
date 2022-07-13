f720ed48d1a6fd01dddb3ffcfcaae36889fba2c4
Gabriel <gabrielx165@gmail.com>
2022-07-13 07:30:27 -0300

    added created_at field on EmailMerketing models

--- app/models/marketing.py
+++ app/models/marketing.py
@@ -1,4 +1,5 @@
 """This module encopassing all the models for marketing management"""
+from datetime import datetime
 
 
 class EmailMarketing:
@@ -9,6 +10,7 @@ class EmailMarketing:
         self.subject = subject
         self.body = body
         self.viwed_by = viwed_by
+        self.created_at = datetime.now()
 
     def __repr__(self):
         return self.id
